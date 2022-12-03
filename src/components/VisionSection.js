c395017a79ad57c137c34ad0a9bc0c0348eb7060
ChengCK18 <chinkheng_98@hotmail.com>
2022-12-03 16:07:16 +0800

    Vis section complete with text

--- src/components/VisionSection.js
+++ src/components/VisionSection.js
@@ -39,28 +39,60 @@ const VisionSection = () => {
     }, [])
 
     return (
-        <div className="h-[400vh] bg-red-400">
+        <div className="h-[400vh]">
             <div className="sticky top-0 flex h-[100vh] h-screen w-full flex-wrap">
-                <div className="flex flex-col bg-blue-100 mobile:h-2/4 mobile:w-full laptop:h-full laptop:w-3/6">
-                    <div className="flex h-4/5 items-center justify-center bg-yellow-200">
-                        <img className="h-2/4 w-3/4" src={sampleImg} />
+                <div className="flex flex-col mobile:h-2/4 mobile:w-full laptop:h-full laptop:w-3/6">
+                    <div className="flex h-4/5 items-center justify-center">
+                        <img className="h-3/4 w-3/4" src={sampleImg} />
                     </div>
-                    <div className=" flex h-1/5 items-center justify-end bg-white font-extrabold mobile:pr-2 mobile:text-5xl  laptop:pr-10 laptop:text-8xl">
-                        <div className="bg-gradient-to-r from-blue-300 to-pink-300 bg-clip-text font-extrabold text-transparent">
+                    <div className=" flex h-1/5 items-center justify-end font-extrabold mobile:pr-2 mobile:text-5xl  laptop:pr-10 laptop:text-8xl">
+                        <div className=" bg-gradient-to-r from-blue-300 to-pink-300 bg-clip-text font-extrabold text-transparent">
                             VISION
                         </div>
                     </div>
                 </div>
-                <div className="flex items-center justify-center bg-blue-200 mobile:h-2/4 mobile:w-2/4 laptop:h-full laptop:w-1/4 ">
-                    <div className="bg-red-300">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
+                <div className="relative flex items-center justify-center mobile:h-2/4 mobile:w-2/4 laptop:h-full laptop:w-1/4 ">
+                    <div
+                        className={`absolute pl-3 text-xl  ${
+                            visionSegment === 0
+                                ? 'opacity-100 transition-opacity duration-700 ease-in '
+                                : ' opacity-0 transition-opacity duration-700 ease-in'
+                        }`}
+                    >
+                        0 Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                    </div>
+                    <div
+                        className={`absolute pl-3 text-xl  ${
+                            visionSegment === 1
+                                ? 'opacity-100 transition-opacity duration-700 ease-in'
+                                : ' opacity-0 transition-opacity duration-700 ease-in'
+                        }`}
+                    >
+                        1 Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                    </div>
+                    <div
+                        className={`absolute pl-3 text-xl ${
+                            visionSegment === 2
+                                ? 'opacity-100 transition-opacity duration-700 ease-in'
+                                : ' opacity-0 transition-opacity duration-700 ease-in'
+                        }`}
+                    >
+                        2 Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
                     </div>
                 </div>
-                <div className="flex flex-col items-center justify-center bg-green-200  mobile:h-2/4 mobile:w-2/4 laptop:h-full laptop:w-1/4 ">
+                <div className="flex flex-col items-center justify-center mobile:h-2/4 mobile:w-2/4 laptop:h-full laptop:w-1/4 ">
                     <div
                         className={`font-extrabold mobile:text-5xl laptop:text-7xl ${
                             visionSegment === 0
