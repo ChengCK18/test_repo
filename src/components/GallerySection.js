a7a395348fe02bb53e0fb671475be8832c7ae531
ChengCK18 <chinkheng_98@hotmail.com>
2022-12-17 18:24:21 +0800

    Revisited gallery implementation

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,16 +1,20 @@
 const GallerySection = () => {
     return (
-        <div className="flex w-full  mobile:h-[180vh] laptop:h-screen">
-            <div className="flex h-full w-4/5 flex-row flex-wrap items-center justify-center space-y-10 pb-5 laptop:space-x-10 laptop:pl-2">
-                <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-1/6 mobile:w-4/5 laptop:h-60 laptop:w-64"></div>
-                <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-1/6 mobile:w-4/5 laptop:h-60 laptop:w-64"></div>
-                <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-1/6 mobile:w-4/5 laptop:h-60 laptop:w-64"></div>
-                <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-1/6 mobile:w-4/5 laptop:h-60 laptop:w-64"></div>
-                <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-1/6 mobile:w-4/5 laptop:h-60 laptop:w-64"></div>
+        <div className="flex h-screen w-screen flex-col">
+            <div className="flex h-3/4 w-screen flex-row justify-center ">
+                <div className="flex items-center  mobile:space-x-10  mobile:overflow-x-auto mobile:pt-44 laptop:h-full  laptop:w-full  laptop:space-y-3 laptop:pb-20">
+                    <div className="flex flex-grow"></div>
+                    <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-60 mobile:min-w-[250px]  laptop:h-80  laptop:w-72"></div>
+                    <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110  mobile:h-60 mobile:min-w-[250px] laptop:h-80  laptop:w-72"></div>
+                    <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-60 mobile:min-w-[250px] laptop:h-80  laptop:w-72"></div>
+                    <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110 mobile:h-60 mobile:min-w-[250px] laptop:h-80  laptop:w-72"></div>
+                    <div className="bg-blue-100 shadow-lg transition ease-in-out hover:scale-110  mobile:h-60 mobile:min-w-[250px] laptop:h-80  laptop:w-72"></div>
+                    <div className="flex flex-grow"></div>
+                </div>
             </div>
 
-            <div className="sticky top-5 flex w-1/12 flex-row items-center justify-center mobile:h-60 laptop:h-full">
-                <div className="-rotate-90 bg-gradient-to-r from-blue-300 to-pink-300 bg-clip-text pb-5 pt-10 font-extrabold text-transparent  mobile:text-5xl laptop:pl-20 laptop:text-8xl">
+            <div className="flex h-1/4 w-full flex-row items-start justify-start ">
+                <div className=" bg-gradient-to-r from-blue-300 to-pink-300 bg-clip-text pb-2 pt-10 font-extrabold text-transparent  mobile:text-6xl laptop:pl-20 laptop:text-8xl">
                     GALLERY
                 </div>
             </div>
